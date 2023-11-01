fb0e34163c2077afbc443a51df06b8af9a73fd07
Fanpion <1524834513@qq.com>
2023-11-01 22:24:46 +0800

    1. 方便新外观上架时, 对数据库补充 2. 将维护工具移动至maintain页面

--- src/main/java/com/fan/wanwindow/controller/WanbaoDailyController.java
+++ src/main/java/com/fan/wanwindow/controller/WanbaoDailyController.java
@@ -1,6 +1,9 @@
 package com.fan.wanwindow.controller;
 
+import com.fan.wanwindow.entity.projection.GoodWpqcAndGfmc;
+import com.fan.wanwindow.repository.GoodInfoMcRepository;
 import com.gao.MainProcess.MainProcess;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/wanbaodaily")
 public class WanbaoDailyController {
 
+    @Autowired
+    GoodInfoMcRepository goodInfoMcRepository;
+
     @PostMapping(value = "/getGoodsTrade")
     public String GetGoodsTrade(String cookie){
         MainProcess.main(new String[]{"2", cookie});
         return "Request Success";
+    }
+
+    @PostMapping(value = "/getGoodDetail")
+    public String getGoodDetail(int updateIDFrom) {
+        for (GoodWpqcAndGfmc goodWpqcAndGfmc : goodInfoMcRepository.getGfmcByIDFrom(updateIDFrom)) {
+            MainProcess.getGoodAnyStatus(goodWpqcAndGfmc.getGfmc());
+        }
+        return "getGoodDetail Success";
     }
 
 }
--- src/main/java/com/fan/wanwindow/repository/GoodInfoMcRepository.java
+++ src/main/java/com/fan/wanwindow/repository/GoodInfoMcRepository.java
@@ -4,6 +4,7 @@ import com.fan.wanwindow.entity.GoodInfoMc;
 import com.fan.wanwindow.entity.projection.GoodWpqcAndGfmc;
 import com.fan.wanwindow.vo.GoodQcAndGfmcVO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +12,7 @@ import java.util.List;
 @Repository
 public interface GoodInfoMcRepository extends JpaRepository<GoodInfoMc, String> {
     List<GoodWpqcAndGfmc> findAllByWpqcLike(String wpmc);
+
+    @Query("from good_info_mc where iD+0 > ?1 ")
+    List<GoodWpqcAndGfmc> getGfmcByIDFrom(int ID);
 }
